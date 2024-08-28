fad141380aec10f2deddc7736e1e5b68a721c47b
shihoyu060507 <139198023+shihoyu060507@users.noreply.github.com>
2024-08-28 19:38:31 +0900

    Create 6030.cpp

---
+++ 6030.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int P,Q;
+  cin >> P >> Q;
+  
+  for(int i=1;i<=P;i++){
+    for(int j=1;j<=Q;j++){
+      if(P%i==0 && Q%j==0){
+        cout << i << " " << j << "\n";
+      }
+    }
+  }  
+  return 0;
+}
