23c6a933cd416064219f19238aa3fdd5a78f9b7a
shihoyu060507 <139198023+shihoyu060507@users.noreply.github.com>
2024-09-04 19:10:19 +0900

    Create 6021.cpp

---
+++ 6021.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int M,F;
+  
+  cin >> M >> F;
+  
+  for(int i=0;i<M;i++){
+    for(int j=0;j<F;j++){
+      
+    }
+  }
+  
+  return 0;
+}
