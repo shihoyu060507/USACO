49382a686b971f767535b659c33056d7d316bf09
shihoyu060507 <139198023+shihoyu060507@users.noreply.github.com>
2024-10-01 20:33:07 +0900

    Create 2748.cpp

---
+++ 2748.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> fib(91);
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    cout << fib[n] << endl;
+
+    return 0;
+}
