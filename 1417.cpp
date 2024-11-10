3c3cf38de2b58cf9a5b8531b57e90fca59b1758a
shihoyu060507 <139198023+shihoyu060507@users.noreply.github.com>
2024-11-10 11:32:55 +0900

    Update 1417.cpp

--- 1417.cpp
+++ 1417.cpp
@@ -24,19 +24,16 @@ int main() {
 		int find_max = 0;
 		int index = 0;
 
-		// 가장 큰 값 찾기
 		for (int i = 0; i < candidate - 1; i++) {
 			if (find_max < vote[i]) {
 				find_max = vote[i];
 				index = i;
 			}
 		}
-		// 찾은 값이 다솜이의 표수보다 적다면 출력
 		if (dasom > find_max) {
 			cout << count << "\n";
 			break;
 		 }
-		// 찾은 값이 다솜이의 표수보다 많다면 뺏어옴
 		dasom++;
 		vote[index]--;
 		count++;
