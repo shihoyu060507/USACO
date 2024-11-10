5fb98c330c55aa07674056ed0be413d6ffc830fb
shihoyu060507 <139198023+shihoyu060507@users.noreply.github.com>
2024-11-10 11:53:12 +0900

    Create 1874.cpp

---
+++ 1874.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,max;
+int cnt=0;
+int seq[100001];
+vector<int> v;
+vector<char> ans;
+int main(){
+  cin >> n;
+  for(int i=0;i<n;i++) cin >> seq[i];
+  
+  for(int i=1;i<=n;i++){
+    v.push_back(i);
+    ans.push_back('+');
+    
+    while(!v.empty() && v.back() == seq[cnt]){
+      v.pop_back();
+      ans.push_back('-');
+      cnt++;
+    }
+  }
+  
+  if(!v.empty()) cout << "NO";
+  else for (int i=0;i<ans.size();i++) cout << ans[i] << '\n';
+  
+  return 0;
+}
